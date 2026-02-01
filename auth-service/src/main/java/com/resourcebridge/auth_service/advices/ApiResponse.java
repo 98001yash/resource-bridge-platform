452d567cd4c7d09120239720905ef064545af236
98001yash <yashchauhan.gaya@gmail.com>
2026-02-01 10:55:02 +0530

    added the hibernate spatial dependency for location management

---
+++ auth-service/src/main/java/com/resourcebridge/auth_service/advices/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.resourcebridge.auth_service.advices;
+
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ApiResponse<T> {
+
+    private LocalDateTime timeStamp;
+    private T data;
+    private ApiError error;
+
+    public ApiResponse(){
+        this.timeStamp = LocalDateTime.now();
+    }
+
+    public ApiResponse(T data){
+        this();
+        this.data = data;
+    }
+
+    public ApiResponse(ApiError error){
+        this();
+        this.error = error;
+    }
+}
